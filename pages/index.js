1856a296e7dcd1a210fa22adb636809b55af7bcf
itc-n23007 <n23007@std.it-college.ac.jp>
2024-01-24 06:23:42 +0000

    chpter1 直し

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 const Home = () => (
   <>
     <header> HEADER </header>
+
     <main>
-      <h1> Cube </h1>
-      <p>アウトプットしていくサイト</p>
+      <div>
+        <h1> Cube </h1>
+        <p>アウトプットしていくサイト</p>
+      </div>
     </main>
+
     <footer>FOOTER</footer>
   </>
 )
